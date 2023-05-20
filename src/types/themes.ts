440068922282cc3efaf7177c5b88dff23d4adab9
Nate <89227631+natedotv2@users.noreply.github.com>
2023-05-20 01:31:52 +0100

    Added types and mockup themes

---
+++ src/types/themes.ts
@@ -0,0 +1,92 @@
+type BackdropTheme = {
+    color: string;
+    opacity?: number | string;
+    backdropFilter?: string;
+    offset?: number | string;
+  };
+  
+  type CommonTheme = {
+    text: {
+      color: string;
+    };
+    backdrop: BackdropTheme;
+  };
+  
+  type PopupTheme = {
+    maxWidth?: number;
+    background: {
+      color: string;
+      backdropFilter?: string;
+    };
+    border: {
+      width: number;
+      color: string;
+      borderRadius: number;
+    };
+    text: {
+      color: string;
+    };
+    closeCross: {
+      color: string;
+      hoverColor?: string;
+      // pressed?: string;
+    };
+    title?: {
+      fontWeight?: string | number;
+    };
+    badgeColor: string;
+    scroll: {
+      color: string;
+    };
+  };
+  
+  type ItemTheme = {
+    background: {
+      color: string;
+    };
+    border: {
+      width: number;
+      borderWidth: number;
+      borderHoverColor?: string;
+      color: string;
+      hoverColor?: string;
+      pressedColor?: string;
+    };
+    text: {
+      color: string;
+    };
+    warning: {
+      background: {
+        color: string;
+      };
+      text: {
+        color: string;
+      };
+    };
+    icon: {
+      main: {
+        color: string;
+      };
+      subTitle: {
+        color: string;
+      };
+    };
+  };
+  
+  export type Theme = {
+    common: CommonTheme;
+    popup: PopupTheme;
+    item: ItemTheme;
+    customItems?: {
+      [item: string]: ItemTheme;
+    };
+  };
+  
+  export type ThemeConfig = {
+    name: string;
+    theme: Theme;
+  };
+  
+  export type ThemesList = {
+    [name: string]: ThemeConfig;
+  };
