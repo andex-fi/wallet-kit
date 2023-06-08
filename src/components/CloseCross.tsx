c1e171939b8f07cd67fd56f2eb88847148cb5f4d
Nate <89227631+natedotv2@users.noreply.github.com>
2023-06-08 05:08:28 +0100

    Bumped version

--- src/components/CloseCross.tsx
+++ src/components/CloseCross.tsx
@@ -34,8 +34,8 @@ export const CloseCross = ({ color, hoverColor, onClick }: CloseCrossProps) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M3.49998 1.37878L4.56064 2.43944L12 9.87878L19.4393 2.43944L20.5 1.37878L22.6213 3.5001L21.5606 4.56076L14.1213 12.0001L21.5606 19.4394L22.6213 20.5001L20.5 22.6214L19.4393 21.5608L12 14.1214L4.56064 21.5608L3.49998 22.6214L1.37866 20.5001L2.43932 19.4394L9.87866 12.0001L2.43932 4.56076L1.37866 3.5001L3.49998 1.37878Z"
         />
       </svg>
